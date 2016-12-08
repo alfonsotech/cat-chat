8e1ba8b680a6b86a6d9c348cb0450c084468708f
lizzkats <katsnelson.lizz@gmail.com>
2016-12-07 18:01:01 -0800

    Adding username functionality

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ var app = express();
 var server = require('http').createServer(app);
 var io = require('socket.io').listen(server);
 
+var nicknames = [];
+
   server.listen(5000);
 
   app.get('/', function(request, response){
@@ -10,6 +12,9 @@ var io = require('socket.io').listen(server);
   });
 
   io.sockets.on('connection', function(socket){
+    socket.on('new user', function(data, callback){
+      //8:35
+    });
     socket.on('send message', function(data) {
       io.sockets.emit('new message', data);
     });
