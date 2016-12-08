d1ca50d5e319e7c0d8b8dfe3bb8b4fb1c9e6b239
lizzkats <katsnelson.lizz@gmail.com>
2016-12-08 11:23:19 -0800

    Adds some styling

--- app.js
+++ app.js
@@ -2,27 +2,32 @@ var express = require('express');
 var app = express();
 var server = require('http').createServer(app);
 var io = require('socket.io').listen(server);
-
 var nicknames = [];
 
-  server.listen(5000);
 
-  app.get('/', function(request, response){
-    response.sendFile(__dirname + '/index.html');
-  });
+server.listen(5000);
 
-  io.sockets.on('connection', function(socket){
-    socket.on('new user', function(data, callback){
-    if (nicknames.indexOf(data) != -1) {
-      callback(false);
-    } else {
-      callback(true);
-      socket.nickname = data;
-      nicknames.push(socket.nickname);
-      io.sockets.emit('usernames', nicknames);
-    }
+
+app.get('/', function(request, response){
+  response.sendFile(__dirname + '/index.html');
+});
+
+// app.use(express.static('public'))
+//
+// app.use('/static', express.static(path.join(__dirname, 'public')))
+
+io.sockets.on('connection', function(socket){
+  socket.on('new user', function(data, callback){
+  if (nicknames.indexOf(data) != -1) {
+    callback(false);
+  } else {
+    callback(true);
+    socket.nickname = data;
+    nicknames.push(socket.nickname);
+    io.sockets.emit('usernames', nicknames);
+  }
+});
+  socket.on('send message', function(data) {
+    io.sockets.emit('new message', data);
   });
-    socket.on('send message', function(data) {
-      io.sockets.emit('new message', data);
-    });
-  });
+});
